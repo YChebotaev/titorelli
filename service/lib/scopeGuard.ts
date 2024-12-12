62cab29b5d2175905bac705ef635fb98cff99d52
ychebotaev <yury.79120345101@gmail.com>
2024-12-12 12:28:08 +0500

    hotfix(service): Fix typo in scope guard

--- service/lib/scopeGuard.ts
+++ service/lib/scopeGuard.ts
@@ -1,5 +1,5 @@
 export const scopeGuard = (clientId: string, modelId: string, scopeSuffix: string, actualScopes: string[]) => {
-  const finalScope = `${modelId}_${scopeSuffix}`
+  const finalScope = `${modelId}/${scopeSuffix}`
 
   if (!actualScopes.includes(finalScope))
     throw new Error(`Client with id = '${clientId}' don't have scope ${finalScope} for this operation`)
